adc3986d88e818d277162687639f42c733ac7556
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-11-22 11:28:32 -0500

    Inital commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    py = pkgs.writeShellScriptBin "py" ''
+      python3 main.py
+    '';
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages =
+        [
+          py
+        ]
+        ++ (with pkgs; [
+          python3
+          python312Packages.pygame-ce
+        ]);
+
+      shellHook = "tmux";
+    };
+  };
+}
